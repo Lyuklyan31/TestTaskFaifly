35c274db484fbf465b116c894cb3fe5f5465a1a9
Oleh <lyuclyan.oleg14@gmail.com>
2025-03-22 16:57:04 +0200

    Created project.

---
+++ TestTaskFaifly/TestTaskFaifly/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TestTaskFaifly
+//
+//  Created by Oleh Liuklian  on 22.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ TestTaskFaifly/TestTaskFaifly/TestTaskFaiflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskFaiflyApp.swift
+//  TestTaskFaifly
+//
+//  Created by Oleh Liuklian  on 22.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskFaiflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
